1d72c494b375b4dc3f4caf26fd5e7d1d24e71230
Eli <elionlink@gmail.com>
2020-08-05 19:24:25 +0300

    050820

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoConnect = require('./mongo/mongoConnect');
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3001;
 const app = express();
 
 mongoConnect();
